2f767df36f462116e72ee1b20695476bced548a7
kevingu24 <kevingu24@gmail.com>
2024-02-22 18:08:40 -0600

    add icon

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -4,6 +4,7 @@ import { Text } from "react-native-rapi-ui";
 import { useTheme } from "../components/navigation/ThemeContext";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function Profile() {
     const { isDarkMode } = useTheme();
@@ -19,6 +20,14 @@ export default function Profile() {
                     <Ionicons name="settings-outline" size={24} color="black" />
                 </TouchableOpacity>
             </View>
+            <View style={styles.iconContainer}>
+                <FontAwesome name="user-circle" size={100} color="grey" />
+                <Text style={styles.iconText}>Joe Miner</Text>
+            </View>
+            <View style={styles.mailIconContainer}>
+                <FontAwesome name="envelope" size={24} color="grey" />
+                <Text style={styles.mailIconText}>joeminer@mst.edu</Text>
+            </View>
         </SafeAreaView>
     );
 }
@@ -26,14 +35,15 @@ export default function Profile() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-      },
+        backgroundColor: "white", // Default light background color
+    },
     darkContainer: {
         backgroundColor: "grey", // Dark background color
     },
     sectionTitle: {
-        fontSize: 30, 
-        fontWeight: "bold", 
-        marginBottom: 7, 
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 7,
         textAlign: "left",
         color: "black", // Default text color
     },
@@ -47,6 +57,25 @@ const styles = StyleSheet.create({
     settingsButton: {
         padding: 10,
         borderRadius: 5,
-        backgroundColor: "lightgray",
+    },
+    iconContainer: {
+        alignItems: "center",
+        marginTop: 30,
+    },
+    iconText: {
+        fontSize: 20,
+        marginTop: 10,
+    },
+    mailIconContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        paddingVertical: 20,
+        width: "100%",
+    },
+    mailIconText: {
+        fontSize: 20,
+        marginLeft: 5,
+        alignSelf: 'center',
     },
 });
