4cce7e219760c1ae2356cd927f43601808c61380
Aidan Nusbaum <aidnus329@gmail.com>
2024-02-26 14:42:31 -0600

    splat of registration from other branch. add integration to tabs next

---
+++ src/components/Separator.jsx
@@ -0,0 +1,35 @@
+import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+
+export default function Separator({text}) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.thinLine}/>
+            <View>
+                <Text style={styles.text}>{text}</Text>
+            </View>
+            <View style={styles.thinLine}/>
+        </View>
+    )
+}
+
+{ /* TODO: remove hardcoded width values */ }
+const styles = StyleSheet.create ({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    }, 
+    thinLine: {
+        flex: 1,
+        borderBottomColor: '#bfbfbf',
+        borderBottomWidth: 1,
+        width: 140,
+        height: 1,
+    },
+    text: {
+        color: '#bfbfbf',
+        fontSize: 20,
+        marginHorizontal: 1,
+    }
+    
+});
