09d86c7e2f1f0bcffff65768ee27b6b6b07f3596
Sudeep Potluri <spotluri20@gmail.com>
2024-03-18 17:52:48 -0500

    Need to decide if we want one model file or not Basic Table complete

--- Backend/DjangoBackend/authentication/models.py
+++ Backend/DjangoBackend/authentication/models.py
@@ -5,3 +5,23 @@ class GroceryList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.TextField()
 
+class List(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    list_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.list_name
+
+class Price(models.Model):
+    product_id = models.IntegerField(primary_key=True) 
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Product ID: {self.product_id}, Price: {self.price}"
+
+class UserGroup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group_id = models.IntegerField()
+
+    def __str__(self):
+        return f"User: {self.user}, Group ID: {self.group_id}"
