2fc2ead8a9ad8f3e3878d0b9e0d0d1ef540362f5
Aidan Nusbaum <aidnus329@gmail.com>
2024-04-07 14:59:09 -0500

    updated context, rebase needed

--- Frontend/components/AuthContext.tsx
+++ Frontend/components/AuthContext.tsx
@@ -120,5 +120,23 @@ export const AuthProvider = ({children} : any) => {
   )
 }
 
+// export function SessionProvider(props: React.PropsWithChildren) {
+//   const [[isLoading, session], setSession] = useStorageState('session');
 
-
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         signIn: () => {
+//           // Perform sign-in logic here
+//           setSession('xxx');
+//         },
+//         signOut: () => {
+//           setSession(null);
+//         },
+//         session,
+//         isLoading,
+//       }}>
+//       {props.children}
+//     </AuthContext.Provider>
+//   );
+// }
