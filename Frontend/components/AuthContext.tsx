0f1bb628f8a1b6f2e068d1622b778151914bc445
Aidan Nusbaum <aidnus329@gmail.com>
2024-04-07 14:59:09 -0500

    updated context, rebase needed

--- Frontend/components/AuthContext.tsx
+++ Frontend/components/AuthContext.tsx
@@ -1,71 +1,123 @@
-import React, { createContext, useState} from 'react';
+import React, { createContext, useState, useEffect, useContext} from 'react';
 import { useStorageState } from './useStorageState';
+import { router } from 'expo-router';
+import axios from 'axios'
+import { deleteItemAsync, setItemAsync, getItemAsync } from 'expo-secure-store'
+const BASE_URL = "http://127.0.0.1:8000";
 
 interface userTokenType {
   token: string;
 }
 
 export interface AuthContextType {
-  login: () => void;
-  logout: () => void;
-  isLoading: boolean;
-  userToken: userTokenType | null;
-
+  onLogin?: (email: string, password: string) => Promise<any>;
+  onLogout?: () => Promise<any>;
+  onRegister?: (email: string, password: string, username: string, firstname: string, lastname: string) => Promise<any>;
+  authState?: {token: string | null; authenticated: boolean | null };
 }
-export const AuthContext = createContext<AuthContextType | null>(null);
+const AuthContext = createContext<AuthContextType>({});
 
-// This hook can be used to access the user info.
-// export function useSession() {
-//   const value = React.useContext(AuthContext);
-//   if (process.env.NODE_ENV !== 'production') {
-//     if (!value) {
-//       throw new Error('useSession must be wrapped in a <SessionProvider />');
-//     }
-//   }
-
-//   return value;
-// }
+export const useAuth = () => {
+  return useContext(AuthContext);
+}
 
 
 
-export function AuthProvider({children} : React.PropsWithChildren<{}>) {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [userToken, setUserToken] = React.useState<userTokenType | null>(null);
 
-  const login = () => {
-    setUserToken({token: "i"});
-    setIsLoading(false);
-  }
+export const AuthProvider = ({children} : any) => {
+  const [authState, setAuthState] = React.useState<{
+    token: string | null;
+    authenticated: boolean | null;
+  }>({
+    token: null,
+    authenticated: null    
+  });
+  
+  useEffect (() => {
+    const loadToken = async () => {
+      const token = await getItemAsync('test');
+      console.log("stored:", token);
 
-  const logout = () => {
-    setUserToken(null);
-    setIsLoading(false);
-  }
+      if (token) {
+        setAuthState({
+          token: token,
+          authenticated: true,
+        });
+      }
+    }
+    loadToken();
+  }, [])
 
+  const login = async (username: string, password: string) => {
+    console.log('login');
+    const result: any = await axios.post(`${BASE_URL}/api/login/`, {
+          username,
+          password
+        })
+        .then(res => {
+            let response = res.data;
+            console.log(response);
+            setAuthState({
+              token: response.token,
+              authenticated: true
+            });
+            return result
+        })
+        .catch(e => {
+            console.log(`login failed ${e}`);
+            { /* FIXME: testing shtuff */}
+            return {error: true, msg: (e as any).response.data.msg}
+        });
+      
+    router.replace('/');
+  };
+  
+
+  const register = async (email: string, password: string, username: string, firstname: string, lastname: string) => {
+    console.log('register');
+    const result: any = await axios.post(`${BASE_URL}/api/register/`, {
+            email,
+            password,
+            username,
+            firstname,
+            lastname
+        })
+        .then(res => {
+            let response = res.data;
+            console.log(response);
+            setAuthState({
+              token: response.token,
+              authenticated: true
+            });
+
+            return result
+        })
+        .catch(e => {
+            console.log(`register failed ${e}`);
+        });
+    router.replace('/login');
+  };
+
+  const logout = async () => {
+    await deleteItemAsync('test');
+    setAuthState({
+      token: null,
+      authenticated: false
+    });
+  };
+
+  const value = {
+    onRegister: register,
+    onLogin: login, 
+    onLogout: logout,
+    authState: authState,
+
+  };
   return (
-    <AuthContext.Provider value={{login, logout, isLoading, userToken}}>
+    <AuthContext.Provider value={value}>
       {children}
     </AuthContext.Provider>
   )
 }
 
-// export function SessionProvider(props: React.PropsWithChildren) {
-//   const [[isLoading, session], setSession] = useStorageState('session');
 
-//   return (
-//     <AuthContext.Provider
-//       value={{
-//         signIn: () => {
-//           // Perform sign-in logic here
-//           setSession('xxx');
-//         },
-//         signOut: () => {
-//           setSession(null);
-//         },
-//         session,
-//         isLoading,
-//       }}>
-//       {props.children}
-//     </AuthContext.Provider>
-//   );
-// }
