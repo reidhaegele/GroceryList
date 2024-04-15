23016f4a2c06436a4974b097170896e4453cd1f9
Reid Haegele <haegelereid@gmail.com>
2024-04-15 15:26:34 -0500

    Kroger API product search with token access

---
+++ Frontend/app/services/krogerAPI.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import Kroger from '@/constants/Kroger';
+
+const CLIENT_ID = Kroger.CLIENT_ID;
+const CLIENT_SECRET = Kroger.CLIENT_SECRET;
+const BASE_URL = Kroger.BASE_URL;
+
+export const requestAccessToken = async () => {
+  const authorization = "Basic " + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+
+  try {
+    console.log(authorization)
+    const response = await axios.post(`${BASE_URL}/connect/oauth2/token`,'grant_type=client_credentials&scope=product.compact',{headers: {'Content-Type': 'application/x-www-form-urlencoded','Authorization': authorization}});
+    return response.data.access_token;
+  } catch (error) {
+    console.error('Error fetching access token:', error);
+    throw error;
+  }
+};
+
+export const fetchStoresNearZip = async (accessToken, zipCode) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/locations?filter.zipCode.near=${zipCode}`,
+      {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Accept': 'application/json'
+        }
+      }
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error('Error fetching stores:', error);
+    throw error;
+  }
+};
+
+export const searchProducts = async (accessToken, query) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/products?filter.term=${query}`,
+      {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Accept': 'application/json'
+        }
+      }
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error('Error searching products:', error);
+    throw error;
+  }
+};
