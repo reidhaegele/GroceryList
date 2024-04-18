35857d52bf07b6b2abe1d6aacf379507b6d92fb6
Aidan Nusbaum <aidnus329@gmail.com>
2024-04-17 22:06:05 -0500

    updated with progress

---
+++ Frontend/app/lists/notlayout.jsx
@@ -0,0 +1,15 @@
+// import { Stack, router, Slot } from 'expo-router';
+
+// export default function Layout() {
+//     return (
+//         <Stack>
+//             <Stack.Screen
+//                 name="add-list"
+//                 screenOptions={{
+//                     headerShown: false,
+//                 }}
+//             />
+//         </Stack>
+            
+//     );
+// }
