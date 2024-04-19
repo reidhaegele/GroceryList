0fbc5dd1763eda421b88547983c588d0598a1095
ldhtnp <ldhtnp@umsystem.edu>
2024-04-19 11:32:09 -0500

    cleaned up

--- Backend/DjangoBackend/EditAccEndpoint/views.py
+++ Backend/DjangoBackend/EditAccEndpoint/views.py
@@ -1,4 +1,3 @@
-#from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authentication import TokenAuthentication
