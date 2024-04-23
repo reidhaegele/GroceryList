36b87a3c0436b9edd1479ee34f211708f1ef4678
Aidan Nusbaum <aanfvd@umsystem.edu>
2024-04-23 17:43:17 -0500

    Issues/43 (#62)
    * added user authentication context
    
    * updated context, rebase needed
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * update readme; python .gitignore
    
    * added usertoken to login request
    
    * adjusted lists. renamed header.
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * update readme; python .gitignore
    
    * added usertoken to login request
    
    * adjusted lists. renamed header.
    
    * updated with progress
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * update readme; python .gitignore
    
    * added usertoken to login request
    
    * adjusted lists. renamed header.
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * updated context, rebase needed
    
    * added usertoken to login request
    
    * adjusted lists. renamed header.
    
    * updated with progress
    
    * updated id cards and page with logic to transform data
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * update readme; python .gitignore
    
    * added usertoken to login request
    
    * adjusted lists. renamed header.
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * updated context, rebase needed
    
    * added usertoken to login request
    
    * adjusted lists. renamed header.
    
    * updated with progress
    
    * updated context, rebase needed
    
    * added user authentication context
    
    * updated context, rebase needed
    
    * updated id cards and page with logic to transform data
    
    * got updated views.py
    
    * corrected type
    
    * updated authcontext; changed login to post request
    
    * removed debugging prints.
    
    * adding to main for now
    
    * corrected constant
    
    * updated to correctly call accountinfo api with authorization token
    
    * working profile page, with update button for now.

--- Frontend/app/(tabs)/profile.jsx
+++ Frontend/app/(tabs)/profile.jsx
@@ -1,25 +1,64 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, StyleSheet, TouchableOpacity, View, Text, Pressable } from "react-native";
 import { useTheme } from "@/components/navigation/ThemeContext";
 import { router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
+import { useAuth } from '@/components/AuthContext';
+import { BASE_URL } from "../../constants/Database";
+import { getItem, getItemAsync, setItemAsync } from 'expo-secure-store';
+import axios from 'axios';
 
 export default function Profile() {
     const { isDarkMode } = useTheme();
+    const { authState } = useAuth(); 
+    const [ userData, setUserData] = useState({firstname: '', lastname: '', email: '', username: ''});
     const goToSettings = () => {
         router.navigate("settings");
     };
+    
+
+    const getAccountInfo = async () => {
+        const token = await getItemAsync('token');
+        const result = await axios.get(`${BASE_URL}/api/accountInfo/`, {
+            headers: {
+                'Authorization': `Token ${token}`
+            }
+        })
+        .then((res) => {
+            console.log(res.data);
+            let data = JSON.parse(res.data)
+            setUserData({firstname: data.first_name, lastname: data.last_name, email: data.email, username: data.username});
+            setItemAsync('userInfo', JSON.stringify(data));
+            return res.data
+
+        })
+        .catch((error) => {
+            console.error(error.response)
+            console.error(error.response.data)
+        });
+        return result; 
+    }
+    
     return (
+
+        
         <SafeAreaView style={[styles.container, isDarkMode && styles.darkContainer]}>
             <View style={styles.iconContainer}>
                 <FontAwesome name="user-circle" size={100} color="grey" />
-                <Text style={[styles.iconText, isDarkMode && styles.darkText]}>Joe Miner</Text>
+                <Text style={[styles.iconText, isDarkMode && styles.darkText]}>{`${userData.firstname} ${userData.lastname}`}</Text>
             </View>
             <View style={styles.mailIconContainer}>
                 <FontAwesome name="envelope" size={24} color="grey" />
-                <Text style={[styles.mailIconText, isDarkMode && styles.darkText]}>joeminer@mst.edu</Text>
+                <Text style={[styles.mailIconText, isDarkMode && styles.darkText]}>{`${userData.email}`}</Text>
             </View>
+            <View style={styles.userIconContainer}>
+                <FontAwesome name="user" size={30} color="grey" />
+                <Text style={[styles.mailIconText, isDarkMode && styles.darkText]}>{`${userData.username}`}</Text>
+            </View>
+            <Pressable style={styles.button} onPress={getAccountInfo}>
+                <Text style={[styles.sectionTitle, isDarkMode && styles.darkText]}>Update Info</Text>
+            </Pressable>
         </SafeAreaView>
     );
 }
@@ -73,4 +112,20 @@ const styles = StyleSheet.create({
         marginLeft: 5,
         alignSelf: 'center',
     },
+
+    userIconContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        paddingVertical: 20,
+        width: "100%",
+    },
+    
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+        borderRadius: 5,
+        margin: 10,
+    }
 });
---
+++ Frontend/components/NewListButton.jsx
@@ -0,0 +1,47 @@
+import { Text, View, Pressable, StyleSheet } from 'react-native';
+import { router } from 'expo-router';
+import axios from 'axios';
+
+BASE_URL = "http://127.0.0.1:8000"
+
+
+export default function NewListButton() {
+
+    const onPress = async () => {
+        console.log("New List Button Pressed")
+        router.navigate('lists/1')
+    }
+
+    return (
+        <Pressable style={styles.button} onPress={onPress}>
+            <Text style={styles.title}>New List +</Text>
+        </Pressable>
+
+    )
+}
+
+
+const styles = StyleSheet.create({
+    button : {
+        flex: 1,
+        backgroundColor: "#F5F4F4",
+        borderRadius: 7,
+        borderColor: "#d9d9d9",
+        width: "90%",
+        justifyContent: 'center',
+        borderWidth: 1,
+        height: 63,
+    },
+
+    title: {
+        fontSize: 18, 
+        textAlign: "center",
+        color: "#787878",
+    },
+    darkContainer: {
+        backgroundColor: "#353535",
+    },
+    darkText: {
+        color: "white",
+    },
+  });
